0e9dfddda1aae069b3d9d15014b3a8e45d53306b
Om Chokse <omchokse30@gmail.com>
2025-03-18 20:45:59 +0530

    Questions based on For Loop

---
+++ For_Loop/factorial.js
@@ -0,0 +1,16 @@
+const num = parseInt(process.argv[2]);
+
+if (isNaN(num) || num < 0) {
+    console.log("Please enter a non-negative integer.");
+    process.exit(1);
+}
+
+function factorial(n) {
+    let result = 1;
+    for (let i = 2; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+console.log(`${num}! = ${factorial(num)}`);
